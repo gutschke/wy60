63977a487ee1e2161049c16fc82a37eb62a937e6
zodiac <zodiac@7dadbc64-7a40-11de-9a63-07504eddd27e>
2009-07-27 00:13:48 +0000

    Version 1.1.2

--- trunk/wy60.h
+++ trunk/wy60.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2001 Markus Gutschke <markus+wy60@wy60.gutschke.com>
+ * Copyright (C) 2001, 2002 Markus Gutschke <markus+wy60@wy60.gutschke.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
